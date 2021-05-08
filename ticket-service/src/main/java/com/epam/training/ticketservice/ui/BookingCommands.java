e9a0c5fc01d5dbbe67cb4683688e94a15f9d45a2
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-05-08 21:26:27 +0200

    feat: add value provider for book command

--- ticket-service/src/main/java/com/epam/training/ticketservice/ui/BookingCommands.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/ui/BookingCommands.java
@@ -2,10 +2,13 @@ package com.epam.training.ticketservice.ui;
 
 import com.epam.training.ticketservice.core.booking.BookingService;
 import com.epam.training.ticketservice.core.user.UserService;
+import com.epam.training.ticketservice.ui.valueproviders.MovieValueProvider;
+import com.epam.training.ticketservice.ui.valueproviders.RoomValueProvider;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellMethodAvailability;
+import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
 public class BookingCommands {
@@ -19,7 +22,9 @@ public class BookingCommands {
     }
 
     @ShellMethod(value = "Book seats for screening.", key = "book")
-    public void create(String movieTitle, String roomName, String start, String seatsString) {
+    public void create(
+            @ShellOption(valueProvider = MovieValueProvider.class) String movieTitle,
+            @ShellOption(valueProvider = RoomValueProvider.class) String roomName, String start, String seatsString) {
         bookingService.create(movieTitle, roomName, start, seatsString);
     }
 
